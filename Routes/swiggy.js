a60be0b27844b80f1e8047706eebff9cc60f813c
Keerthi Narayana <keerthinarayanaprasad2009@gmail.com>
2020-05-26 20:35:55 +0530

    images , menu.handlebars added and home.handlebars updated

--- Routes/swiggy.js
+++ Routes/swiggy.js
@@ -4,6 +4,10 @@ const router = require("express").Router();
 /*----------------Swiggy GET stats----------------*/
 router.get("/restaurants",(req,res) => {
     res.render("./swiggy/restaurants");
+});
+
+router.get("/menu",(req,res) => {
+    res.render("./swiggy/menu");
 })
 
 /*------------------Swiggy GET ends--------------*/
